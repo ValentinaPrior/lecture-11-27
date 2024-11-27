252453eb57a0b43d0520e0c88f21edc43818863e
ValentinaPrior <vbp7@njit.edu>
2024-11-27 09:37:34 -0500

    edits

---
+++ lecture.js
@@ -0,0 +1,21 @@
+function AddNumbers()
+{
+    let a=100;
+    let b=200;
+    document.getElementById('heading-two').innerText=(a+b);
+    document.getElementById('para1').innerHTML="<h1>New Heading one</h1>";
+}
+function getInput()
+{
+    let usernmae= document.getElementById('username').value;
+    document.getElementById('heading-three').innerText="hello, "+ username;
+}
+document.getElementById('btn1').addEventListener('click',getInput)
+{
+        let fName="Harry";
+        letpassWord="Potter";
+        document.getElementById
+}
+
+
+window.location.href="test.html";
